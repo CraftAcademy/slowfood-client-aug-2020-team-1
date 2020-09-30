6e8a0a954897f8c3367d56fbd9a11177a09ed33b
oliverochman <oliver.ochman@hotmail.com>
2020-09-30 15:46:28 +0200

    add stripe provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from "axios";
 import * as serviceWorker from './serviceWorker';
+import { StripeProvider } from 'react-stripe-elements'
 
 axios.defaults.baseURL = 'http://localhost:3000/api/v1';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StripeProvider apiKey="pk_test_21nBNjeqdyB1Mzm2VjDPQprF00kyEKYZSK">
+    <App />
+  </StripeProvider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
